4886c5e951005c7ce4a1a23101d49476361eb01b
cristian <127469110+NarvaezSKY@users.noreply.github.com>
2023-05-10 23:30:42 -0500

    añadidos nuevos ejercicios con el ciclo for

---
+++ experimentos_xd/ejercicios-practica.js
@@ -0,0 +1,13 @@
+// let valor = parseInt(prompt("Ingresa el valor"));
+
+// for (let i=0; i<=valor;i++){
+// document.write(`${i},`);
+
+// };
+
+let valor = parseInt(prompt("Ingresa el valor"));
+
+for (let i=valor; i>0;i--){
+console.log(`${i},`);
+
+};
